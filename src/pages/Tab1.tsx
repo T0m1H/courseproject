f0b479e15a13fefd7c22d9482e254af8335c0c54
T0m1H <tomi.t.haukka@gmail.com>
2025-09-25 20:20:57 +0300

    rakentelua

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,25 +1,59 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonApp,
+  IonContent,
+  IonHeader,
+  IonMenu,
+  IonMenuToggle,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Mobiilisovellus</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
+    <IonApp>
+      <IonMenu contentId="main-content">
+        <IonHeader>
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle>Valikko</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
-    </IonPage>
+        <IonContent>
+          <IonList>
+            <IonListHeader>Siirry sivulle</IonListHeader>
+            <IonMenuToggle>
+              <IonItem routerLink="/service" routerDirection="forward">
+                <IonLabel>Service</IonLabel>
+              </IonItem>
+            </IonMenuToggle>
+          </IonList>
+        </IonContent>
+      </IonMenu>
+
+      <IonPage id="main-content">
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Mobiilisovellus</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
+          <IonHeader collapse="condense">
+            <IonToolbar>
+              <IonTitle size="large">Tab 1</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <ExploreContainer name="Tab 1 page" />
+        </IonContent>
+      </IonPage>
+    </IonApp> 
   );
 };
 
 export default Tab1;
+
