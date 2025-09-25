a49955cfefd54cb1c4217ed6f9f5284669df1b2d
T0m1H <tomi.t.haukka@gmail.com>
2025-09-25 19:59:43 +0300

    nimi

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -7,7 +7,7 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Testi</IonTitle>
+          <IonTitle>Mobiilisovellus</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
